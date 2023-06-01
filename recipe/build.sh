76d1295a193f855c29541fdd77470ef98114989a
Pavel Zwerschke <pavelzw@gmail.com>
2023-06-01 20:36:31 +0200

    wip

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,10 +2,9 @@
 
 mkdir build && cd build
 
-cmake ${CMAKE_ARGS} -LAH \
+cmake ${CMAKE_ARGS} .. -LAH \
     -DCMAKE_BUILD_TYPE=Release \
     -DCMAKE_INSTALL_LIBDIR=lib \
-    -DCMAKE_INSTALL_PREFIX=${PREFIX} \
-    ..
+    -DCMAKE_INSTALL_PREFIX=${PREFIX}
 
 cmake --build . --target install -j${CPU_COUNT}
