0a2841e4f9ff39cc728e5f41faa6a9312c2678d8
Pavel Zwerschke <pavelzw@gmail.com>
2023-06-06 10:21:00 +0200

    Fix cmake, revert pin

--- recipe/build.sh
+++ recipe/build.sh
@@ -4,11 +4,15 @@ cd source
 
 mkdir build
 
+if [[ "${target_platform}" == linux-* ]]; then
+  export LDFLAGS="-lrt ${LDFLAGS}"
+fi
+
 cmake -B build \
     ${CMAKE_ARGS} -LAH \
-    -DCMAKE_C_FLAGS=-lrt \
+    -DCMAKE_C_COMPILER=${CC} \
+    -DCMAKE_CXX_COMPILER=${CXX} \
     -DCMAKE_BUILD_TYPE=Release \
-    -DCMAKE_INSTALL_LIBDIR=lib \
     -DCMAKE_INSTALL_PREFIX=${PREFIX}
 
 cmake --build build/ --target install -j${CPU_COUNT}
