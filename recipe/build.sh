2dd7f8b1a8ddaa448cf1253081d9a797693605c7
Pavel Zwerschke <pavelzw@gmail.com>
2023-06-07 12:34:43 +0200

    test

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,17 +2,27 @@
 
 cd source
 
-mkdir build
-
 if [[ "${target_platform}" == linux-* ]]; then
   export LDFLAGS="-lrt ${LDFLAGS}"
 fi
 
-cmake -B build \
-    ${CMAKE_ARGS} -LAH \
-    -DCMAKE_C_COMPILER=${CC} \
-    -DCMAKE_CXX_COMPILER=${CXX} \
-    -DCMAKE_BUILD_TYPE=Release \
-    -DCMAKE_INSTALL_PREFIX=${PREFIX}
+cmake -B build -LAH \
+      -DCMAKE_BUILD_TYPE="Release"  \
+      -DCMAKE_PREFIX_PATH=${PREFIX} \
+      -DCMAKE_INSTALL_LIBDIR=lib \
+      -DCMAKE_INSTALL_PREFIX=${PREFIX} \
+      -Wno-dev
 
-cmake --build build/ --target install -j${CPU_COUNT}
+
+cmake --build build -j${CPU_COUNT}
+cmake --install build
+
+
+# cmake -B build \
+#     ${CMAKE_ARGS} -LAH \
+#     -DCMAKE_C_COMPILER=${CC} \
+#     -DCMAKE_CXX_COMPILER=${CXX} \
+#     -DCMAKE_BUILD_TYPE=Release \
+#     -DCMAKE_INSTALL_PREFIX=${PREFIX}
+
+# cmake --build build/ --target install -j${CPU_COUNT}
