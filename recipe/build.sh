2032aa887e21ea3e04de5d59d10223afbe9b3653
Pavel Zwerschke <pavelzw@gmail.com>
2023-06-07 13:18:40 +0200

    .

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,16 +1,17 @@
-#!/usr/bin/env bash
-
+#!/bin/bash
 cd source
 
 if [[ "${target_platform}" == linux-* ]]; then
     export LDFLAGS="-lrt ${LDFLAGS}"
 fi
 
+echo $PREFIX
+
 cmake -B build -LAH \
       -DCMAKE_BUILD_TYPE="Release"  \
-      -DCMAKE_PREFIX_PATH=${PREFIX} \
+      -DCMAKE_PREFIX_PATH=$PREFIX \
       -DCMAKE_INSTALL_LIBDIR=lib \
-      -DCMAKE_INSTALL_PREFIX=${PREFIX} \
+      -DCMAKE_INSTALL_PREFIX=$PREFIX \
       -Wno-dev
 
 
